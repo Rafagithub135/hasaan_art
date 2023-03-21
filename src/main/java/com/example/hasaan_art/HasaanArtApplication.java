d5df2fa481f0fc6f3d3687450ce3a237c0e09820
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-03-20 22:35:42 -0400

    initial configuration

--- src/main/java/com/example/hasaan_art/HasaanArtApplication.java
+++ src/main/java/com/example/hasaan_art/HasaanArtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HasaanArtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HasaanArtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HasaanArtApplication.class, args);
+    }
 
 }
