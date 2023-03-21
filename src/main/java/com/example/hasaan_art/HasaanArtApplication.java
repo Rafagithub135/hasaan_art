a4d87cb961a26a1b44aea3d9b3caf5e99dc1114b
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-03-20 22:34:00 -0400

    first commit

---
+++ src/main/java/com/example/hasaan_art/HasaanArtApplication.java
@@ -0,0 +1,13 @@
+package com.example.hasaan_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasaanArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasaanArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hasaan_art/HasaanArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hasaan_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasaanArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
